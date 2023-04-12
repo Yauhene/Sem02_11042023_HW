d30be02a432385671ee01290d4639a12fffe3535
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-12 21:42:53 +0300

    task 14 done

---
+++ main14.py
@@ -0,0 +1,14 @@
+﻿# Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+
+print()
+N = int(input("Ведите целое число : "))
+flag = True
+n = 0
+while flag:
+    result = 2**(n)
+    if result <= N:
+        print(f' Два в степени {n} = {result};')
+        n += 1
+    else:
+        flag = False
+print()
